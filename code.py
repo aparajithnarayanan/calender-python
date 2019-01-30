e00498c1d51d586d0e68ebe395469b29cdaffbdf
aparajithnarayanan <47037354+aparajithnarayanan@users.noreply.github.com>
2019-01-30 20:12:08 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
+#This program creates a calender
 import calendar
 
-yy = 2019
-mm = 3
+yy = 2019 #Enter the year you want
+mm = 3    #Enter the month you want
  yy = int(input("Enter year: "))
  mm = int(input("Enter month: "))
 print(calendar.month(yy, mm))
